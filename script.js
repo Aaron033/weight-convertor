5ae5f4469f6dc80c994fd596a0088188cad0e237
Aaron <aaron.botello.h@gmail.com>
2019-03-03 22:51:55 -0600

    Adding javascript code

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+document.getElementById('lbsInput').addEventListener('input', function(e){
+   let pounds = e.target.value; 
+    console.log(pounds);
+})
+//We lisent for an input event
+////function(e){
+//   let pounds = e.target.value; 
+//    console.log(pounds);  it's going to output whatever number we input in the console 
