68849d5ac90550e2f5a7b7491b22f3302326b61f
Aaron <aaron.botello.h@gmail.com>
2019-03-05 21:14:15 -0600

    flex-direction not working

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ document.getElementById('lbsInput').addEventListener('input', function(e){
     document.getElementById('gramsOutput').innerHTML = (lbs/0.0022046).toFixed(2);
     document.getElementById('kgOutput').innerHTML = (lbs/2.2046).toFixed(2);
     document.getElementById('ozOutput').innerHTML = lbs*16;
+    
 })
+
+
 //We lisent for an input event
 ////function(e){
 //   let pounds = e.target.value; 
