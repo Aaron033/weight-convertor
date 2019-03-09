de9c563683b8424c92155412d32706503c9048e8
Aaron <aaron.botello.h@gmail.com>
2019-03-09 16:49:53 -0600

    lat and long using coords built-in

--- weather/app.js
+++ weather/app.js
@@ -2,6 +2,12 @@ window.addEventListener('load', () =>{
     let long; 
     let lat; 
     if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(position =>{
+       
+            //position can be any name 
+            long = position.coords.longitude; 
+            lat = position.coords.latitude;
+        });
         
     }else {
         h1.textContent = "hey this is not working "
