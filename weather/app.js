352cb5d9093da4fe45d30cec66a7c72bbb41389d
Aaron <aaron.botello.h@gmail.com>
2019-03-09 16:32:11 -0600

    app.js code added

---
+++ weather/app.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', () =>{
+    let long; 
+    let lat; 
+    if(navigator.geolocation){
+        
+    }else {
+        h1.textContent = "hey this is not working "
+    }
+});
+
+//load is built in function
