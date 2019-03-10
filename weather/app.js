95f31bb56aaade3d291146ae432c0ddb5747b3e9
Aaron <aaron.botello.h@gmail.com>
2019-03-09 21:31:18 -0600

    working with app.js api

--- weather/app.js
+++ weather/app.js
@@ -7,7 +7,15 @@ window.addEventListener('load', () =>{
             //position can be any name 
             long = position.coords.longitude; 
             lat = position.coords.latitude;
+            const api = `https://api.darksky.net/forecast//${lat},${long}`;
+            
+            
+            fetch(api)
+        .then(response =>{
+            return response.json(); //its going to convert to json 
+        })
         });
+        
         
     }else {
         h1.textContent = "hey this is not working "
